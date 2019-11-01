7d37f32dc08074b7bcb8ad8dc1577f075600ab5f
Michael Wells <mwells1286@gmail.com>
2019-11-01 13:30:06 -0700

    fix number to string bug.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-userNum="4"
+userNum="42"
 function countUp(userNum){
   var numArray=[];
   for(i = 0; i <= userNum; i+=1){
@@ -7,27 +7,31 @@ function countUp(userNum){
   }
   return numArray;
 }
-var resultsString = countUp(userNum).join('')
-var countUpResults = resultsString.split();
 
+
+var countUpResults = (countUp(userNum).join().split(","));
 
 // var countUpResults = ["1", "4", "3"];
 function replaceNums(countUpResults){
-
   var replaceArray = [];
-  var testArray = ['3', '2', '1'];
+  var testArray = ["3", "2", "1"];
   var resultsArray =["I'm sorry Dave. I'm afraid I can't do that.", "Boop!", "Beep!"];
   countUpResults.forEach(function(result){
     // result.split('');
     for(i=0;i<result.length;i++){
-      if(result[i] === testArray[0]){
-      replaceArray.push(resultsArray[0]);
-    } else {
-      replaceArray.push(result[i]);
+      if(result === testArray[0]){
+        replaceArray.push(resultsArray[0]);
+      } else if(result === testArray[1]){
+        replaceArray.push(resultsArray[1]);
+      } else if(result === testArray[2]){
+        replaceArray.push(resultsArray[2]);
+
+      } else {
+        replaceArray.push(result[i]);
+      };
     };
-  };
-});
-return replaceArray;
+  });
+  return replaceArray;
 }
 
 var replaceResults = replaceNums(countUpResults);
