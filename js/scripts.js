c11ab281451380a0e006f8f5ae8dfae4d3a650f8
Michael Wells <mwells1286@gmail.com>
2019-11-01 09:48:55 -0700

    add functionality to return through correct prioritization of numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
-numUser = ["3", "4", "5", "3", "5", "2", "1", "4"]
-testArray = ['3', '2', '1']
-results = ''
-for(j = 0; j<numUser.length; j++) {
-if(numUser[j] == testArray[0]) {
-results = "I'm sorry Dave. I'm afraid I can't do that."
-break;
-} else if (numUser[j] == testArray[1]) {
-results = "Boop!";
-}else if (numUser[j] == testArray[2]) {
-results = "Beep!"
-} else {
-numUser[j]=numUser[j];
+var numUser = ["2", "4", "5", "3", "5", "2", "1", "4"];
+var testArray = ['3', '2', '1'];
+var results;
+var resultsArray =["I'm sorry Dave. I'm afraid I can't do that.", "Boop!", "Beep!"];
+for(var j = 0; j<numUser.length; j++) {
+  if(numUser[j] == testArray[2]) {
+    results = 2
+    break;
+  } else {
+    numUser[j]=numUser[j];
+  }
+}
+for(var j = 0; j<numUser.length; j++) {
+  if(numUser[j] == testArray[1]) {
+    results = 1
+    break;
+  } else {
+    numUser[j]=numUser[j];
+  }
 
 }
-console.log(numUser)
+for(var j = 0; j<numUser.length; j++) {
+  if(numUser[j] == testArray[0]) {
+    results = 0
+    break;
+  } else {
+    numUser[j]=numUser[j];
+  }
 }
+
+finalResults = resultsArray[results];
+console.log(finalResults)
