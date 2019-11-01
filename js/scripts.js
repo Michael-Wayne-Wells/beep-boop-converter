2ae3a13e4d45aa914a0052d15a88cbd40ed26eaa
Michael Wells <mwells1286@gmail.com>
2019-11-01 14:30:23 -0700

    add functionality to checking and replacing 1's 2's and 3's regardless of number length.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
+var userNum="332"
 
-userNum="42"
 function countUp(userNum){
   var numArray=[];
   for(i = 0; i <= userNum; i+=1){
@@ -7,34 +7,43 @@ function countUp(userNum){
   }
   return numArray;
 }
-
-
 var countUpResults = (countUp(userNum).join().split(","));
 
-// var countUpResults = ["1", "4", "3"];
 function replaceNums(countUpResults){
   var replaceArray = [];
   var testArray = ["3", "2", "1"];
   var resultsArray =["I'm sorry Dave. I'm afraid I can't do that.", "Boop!", "Beep!"];
   countUpResults.forEach(function(result){
-    // result.split('');
-    for(i=0;i<result.length;i++){
-      if(result === testArray[0]){
-        replaceArray.push(resultsArray[0]);
-      } else if(result === testArray[1]){
-        replaceArray.push(resultsArray[1]);
-      } else if(result === testArray[2]){
-        replaceArray.push(resultsArray[2]);
-
+    var numberTest1 = false;
+    var numberTest2 = false;
+    var numberTest3 = false;
+    for(var j=0;j<result.length;j+=1){
+      if(result[j] === testArray[0]){
+        numberTest1 = true;
+      } else if(result[j] === testArray[1]){
+        numberTest2=true;
+      } else if(result[j] === testArray[2]){
+        numberTest3=true;
       } else {
-        replaceArray.push(result[i]);
+        result[j]=result[j];
+
       };
     };
+    if(numberTest1 === true){
+      replaceArray.push(resultsArray[0]);
+    } else if(numberTest2 === true && numberTest1 === false) {
+      replaceArray.push(resultsArray[1]);
+    } else if (numberTest3 === true && numberTest1 === false && numberTest2 === false) {
+      replaceArray.push(resultsArray[2]);
+    } else {
+      replaceArray.push(result);
+    };
+    return replaceArray;
   });
   return replaceArray;
-}
-
+};
 var replaceResults = replaceNums(countUpResults);
-console.log(replaceResults);
 
+console.log(replaceResults);
 //test
+userNum="332"
