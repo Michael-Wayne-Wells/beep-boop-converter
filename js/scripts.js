8cf44d74d14cebe733a5452a00bce834bfe0dc50
Michael Wells <mwells1286@gmail.com>
2019-11-01 12:51:21 -0700

    add function to replace three with "I'm sorry dave"

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
 
+userNum="4"
 function countUp(userNum){
-var numArray=[];
-for(i = 0; i <= userNum; i+=1){
-  numArray.push(i);
-}
-return numArray;
+  var numArray=[];
+  for(i = 0; i <= userNum; i+=1){
+    numArray.push(i);
+  }
+  return numArray;
 }
 
-userNum=4
+var countUpResults = countUp(userNum);
 
-console.log(countUp(userNum));
+console.log(countUpResults);
+// var countUpResults = ["1", "4", "3"];
+function replaceNums(countUpResults){
+  var testArray = ['3', '2', '1'];
+  var resultsArray =["I'm sorry Dave. I'm afraid I can't do that.", "Boop!", "Beep!"];
+  countUpResults.forEach(function(result){
+    result.split('');
+    for(i=0;i<result.length;i++){
+      if(result[i] === testArray[0]){
+      result = resultsArray[0];
+    } else {
+      return result[i];
+    };
+  };
+});
+}
+
+var replaceResults = replaceNums(countUpResults);
+console.log(replaceResults);
+
+//test
